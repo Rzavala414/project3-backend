6c65b53fcaad792353227372dfad5f37ec671bc3
danlgrigg <griggdl@gmail.com>
2019-11-01 19:08:46 -0700

    Added routes to controller/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,18 @@ const router = require("express").Router();
 //TODO:Make those routes connect to our mongo database
 //TODO:have the data base update when a new game is played
 
+
+module.exports = router;
+
+
+const express = require('express');
+const router = express.Router();
+
+const authRoutes = require('./auth');
+const allRoutes = require("./allRoutes.js")
+
 router.get("/",(res,req)=>res.json("hello connected"))
-    
+router.use('/auth',authRoutes);
+router.use("/allRoutes",allRoutes);
+
 module.exports = router;
